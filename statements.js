8ebf7e24b55302789ae9ce741e8fd060b06aa5b4
Snehal <snehalgidde1998@gmail.com>
2021-12-24 12:49:59 +0530

    TypeScript if--else Statements

---
+++ statements.js
@@ -0,0 +1,10 @@
+var num = 20;
+if (num == 10) {
+    console.log("Typescript Statement:10");
+}
+else if (num == 20) {
+    console.log("Typescript Statement:20");
+}
+else {
+    console.log("Typescript Statement:n");
+}
